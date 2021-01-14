aaf5a89a8ef41ac1817f06ebeccf0a7f8dfc0e35
lizzymejia <72759970+lizzymejia@users.noreply.github.com>
2021-01-14 14:55:31 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 while True:
-    if input.sound_level() < 100:
-        music.play_melody("C# C# e D#       C#   C# e D#    ", 183)
-        music.play_melody("C# C#           C#   C#         ", 183)
-        music.play_melody("C# C#           C#   C#         ", 183)
+    if input.light_level() > 30:
+        music.play_melody("C# C# E D# B B C# C# E D# B", 183)
+        music.play_melody("G E F# D# E C# D# B", 183)
+        music.play_melody("G E F# D# E D# C# B", 183)
+        music.play_melody("B5 G A F# G E F# D#", 183)
+        music.play_melody("B5 G A F# G F# E D#", 183)
+        music.play_melody("B C# A# B G# A# F#", 183)
+        music.play_melody("D# B C# A# B A# G# F#", 183)
+        music.play_melody("D# B C# A# B G# A# F#", 183)
+        music.play_melody("D# B C# A# B G# A# F#", 183)
+   
--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
 while (true) {
-    if (input.soundLevel() < 100) {
-        music.playMelody("C# C# e D#       C#   C# e D#    ", 183)
-        music.playMelody("C# C#           C#   C#         ", 183)
-        music.playMelody("C# C#           C#   C#         ", 183)
+    if (input.lightLevel() > 30) {
+        music.playMelody("C# C# E D# B B C# C# E D# B", 183)
+        music.playMelody("G E F# D# E C# D# B", 183)
+        music.playMelody("G E F# D# E D# C# B", 183)
+        music.playMelody("B5 G A F# G E F# D#", 183)
+        music.playMelody("B5 G A F# G F# E D#", 183)
+        music.playMelody("B C# A# B G# A# F#", 183)
+        music.playMelody("D# B C# A# B A# G# F#", 183)
+        music.playMelody("D# B C# A# B G# A# F#", 183)
+        music.playMelody("D# B C# A# B G# A# F#", 183)
     }
     
 }
