25d5bdbdf3684f4bfd42d44eb3201ea2d53fbef4
lizzymejia <72759970+lizzymejia@users.noreply.github.com>
2021-01-14 13:09:52 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+while True:
+    if input.sound_level() < 100:
+        music.play_melody("C# C# e D#       C#   C# e D#    ", 183)
+        music.play_melody("C# C#           C#   C#         ", 183)
+        music.play_melody("C# C#           C#   C#         ", 183)
---
+++ main.ts
@@ -0,0 +1,8 @@
+while (true) {
+    if (input.soundLevel() < 100) {
+        music.playMelody("C# C# e D#       C#   C# e D#    ", 183)
+        music.playMelody("C# C#           C#   C#         ", 183)
+        music.playMelody("C# C#           C#   C#         ", 183)
+    }
+    
+}
